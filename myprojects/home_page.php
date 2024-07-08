c76e36dc24d7d20277b94ac1d56009de104f5aff
OMEGAxOMV <141241701+OMEGAxOMV@users.noreply.github.com>
2024-07-08 15:41:52 +0530

    File uploaded .

---
+++ myprojects/home_page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Header with Navigation Bar</title>
+    <link rel="stylesheet" href="home_page.css">
+</head>
+
+<body>
+    <header>
+        <a href="#" class="logo">Logo</a>
+        <ul class="nav-items">
+            <li class="nav-item"><a href="#">Home</a></li>
+            <li class="nav-item"><a href="#">About</a></li>
+            <li class="nav-item"><a href="#">Contaact</a></li>
+            <li class="nav-item"><a href="#">Services</a></li>
+        </ul>
+        <a href="#" class="login-button">Login</a>
+    </header>
+    <div class="home">
+        <div class="content">
+            <h2>Welcome To EV Finder</h2>
+            <h1>A Charging Station Finder </h1>
+            <h1>For Your EV</h1>
+            <button ><a href="index.php">Find Charging Station</a></button>
+        </div>
+    </div>
+</body>
+
+</html>
